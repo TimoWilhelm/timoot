d7c96e2d9fddcf27f1e41a662e0ac7d4135dd302
Timo Wilhelm <mail@timowilhelm.com>
2025-12-26 00:54:41 +0100

    fix dialog overlay

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -42,21 +42,23 @@ const DialogContent = React.forwardRef<
 	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
 	<DialogPortal>
-		<DialogOverlay className="flex h-dvh items-center justify-center overflow-y-auto p-4 transition-[height] duration-200 ease-out">
-			<DialogPrimitive.Content
-				ref={ref}
-				className={cn(
-					'relative z-50 grid w-full max-w-[calc(100vw-2rem)] gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:max-w-lg',
-					className,
-				)}
-				{...props}
-			>
-				{children}
-				<DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-					<X className="h-4 w-4" />
-					<span className="sr-only">Close</span>
-				</DialogPrimitive.Close>
-			</DialogPrimitive.Content>
+		<DialogOverlay>
+			<div className="flex h-dvh items-center justify-center overflow-y-auto p-4 transition-[height] duration-200 ease-out">
+				<DialogPrimitive.Content
+					ref={ref}
+					className={cn(
+						'relative z-50 grid w-full max-w-[calc(100vw-2rem)] gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:max-w-lg',
+						className,
+					)}
+					{...props}
+				>
+					{children}
+					<DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+						<X className="h-4 w-4" />
+						<span className="sr-only">Close</span>
+					</DialogPrimitive.Close>
+				</DialogPrimitive.Content>
+			</div>
 		</DialogOverlay>
 	</DialogPortal>
 ));
