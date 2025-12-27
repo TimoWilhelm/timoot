738de7b66298e3eb41a2d22b5457c4c6876bc88a
Timo Wilhelm <mail@timowilhelm.com>
2025-12-27 15:45:04 +0100

    add lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+
+export default {
+	'*': () => 'bun run knip',
+	'*.{ts,tsx}': (stagedFiles) => [
+		'bun run typecheck',
+		`prettier --list-different ${stagedFiles.join(' ')}`,
+		`eslint ${stagedFiles.join(' ')}`,
+	],
+	'*.md': (stagedFiles) => `prettier --list-different ${stagedFiles.join(' ')}`,
+};
+
