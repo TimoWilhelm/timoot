e2ec4dd1bfbd06d893fb056083b18f6301425bb8
Timo Wilhelm <mail@timowilhelm.com>
2026-02-09 22:52:27 +0100

    fix tests

--- test/unit/environment.d.ts
+++ test/unit/environment.d.ts
@@ -1,4 +1,4 @@
 declare module 'cloudflare:test' {
-	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-	interface ProvidedEnvironment extends Env {}
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type, unicorn/prevent-abbreviations
+	interface ProvidedEnv extends Env {}
 }
